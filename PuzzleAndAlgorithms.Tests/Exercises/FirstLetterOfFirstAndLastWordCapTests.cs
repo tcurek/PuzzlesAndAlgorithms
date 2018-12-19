c996ca6e3fe35a4a7214983b61bf8d1b6fc0c700
Tim Curek <tim.curek@almacgroup.com>
2018-12-19 00:10:41 -0500

    Completed first exercise

---
+++ PuzzleAndAlgorithms.Tests/Exercises/FirstLetterOfFirstAndLastWordCapTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesAndAlgorithms.Exercises;
+
+namespace PuzzleAndAlgorithms.Tests.Exercises
+{
+    [TestClass()]
+    public class FirstLetterOfFirstAndLastWordCapTests
+    {
+        [TestMethod()]
+        public void VerifyFirstLetterOfFirstAndLastWordCapCall()
+        {
+            var input = "hI MY naME is tIm.";
+            var expected = "Hi my name is Tim.";
+
+            var sut = new FirstLetterOfFirstAndLastWordCap();
+            Assert.AreEqual(expected, sut.CapitalizeFirstLetterOfFirstAndLastWord(input));
+        }
+
+        [TestMethod()]
+        public void VerifyFirstLetterOfFirstAndLastWordCap2()
+        {
+            var input = "HI MY naME is TIm.";
+            var expected = "Hi my name is Tim.";
+
+            var sut = new FirstLetterOfFirstAndLastWordCap();
+            Assert.AreEqual(expected, sut.CapitalizeFirstLetterOfFirstAndLastWord(input));
+        }
+
+        [TestMethod()]
+        public void VerifyFirstLetterOfFirstAndLastWordCapWhenNull()
+        {
+            string input = null;
+            var sut = new FirstLetterOfFirstAndLastWordCap();
+            Assert.IsNull(sut.CapitalizeFirstLetterOfFirstAndLastWord(input));
+        }
+    }
+}
---
+++ PuzzlesAndAlgorithms/Exercises/FirstLetterOfFirstAndLastWordCap.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace PuzzlesAndAlgorithms.Exercises
+{
+    public class FirstLetterOfFirstAndLastWordCap
+    {
+        /*
+         * Capitalize first letter of first word.
+         * Capitalize first letter of last word.
+         * All other words/letters should be lower cased.
+         */
+
+        public string CapitalizeFirstLetterOfFirstAndLastWord(string initial)
+        {
+            if (string.IsNullOrEmpty(initial))
+                return initial;
+
+            initial = initial.ToLower();
+            var listOfWords = initial.Split(" ");
+            var finalOutput = new StringBuilder();
+
+            finalOutput.Append(char.ToUpper(listOfWords[0][0]) + listOfWords[0].Substring(1));
+
+            for (int i = 1; i < listOfWords.Length - 1; i++)
+            {
+                finalOutput.Append(" " + listOfWords[i]);
+            }
+
+            finalOutput.Append(" ");
+            finalOutput.Append(char.ToUpper(listOfWords[listOfWords.Length - 1][0]) + listOfWords[listOfWords.Length - 1].Substring(1));
+
+            return finalOutput.ToString();
+        }
+    }
+}
