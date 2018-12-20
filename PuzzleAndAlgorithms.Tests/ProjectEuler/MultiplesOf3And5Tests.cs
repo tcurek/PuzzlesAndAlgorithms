ec104e14bd35adc69792843faf282f6026bb49d6
Tim Curek <tim.curek@almacgroup.com>
2018-12-19 20:29:06 -0500

    Project Euler Thread 1. Mult of 3 and 5

---
+++ PuzzleAndAlgorithms.Tests/ProjectEuler/MultiplesOf3And5Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesAndAlgorithms.ProjectEuler;
+
+namespace PuzzlesAndAlgorithms.Tests.ProjectEuler
+{
+    [TestClass()]
+    public class MultiplesOf3And5Tests
+    {
+        [TestMethod()]
+        public void GetMultiplesOf3And5WithZeroBounds()
+        {
+            var sut = new MultiplesOf3And5();
+            Assert.AreEqual(0, sut.GetMultiplesOf3And5(0));
+        }
+
+        [TestMethod()]
+        public void GetMultiplesOf3And5With10Bounds()
+        {
+            var sut = new MultiplesOf3And5();
+            Assert.AreEqual(23, sut.GetMultiplesOf3And5(10));
+        }
+
+        [TestMethod()]
+        public void GetMultiplesOf3And5With1000Bounds()
+        {
+            var sut = new MultiplesOf3And5();
+            Assert.AreEqual(233168, sut.GetMultiplesOf3And5(1000));
+        }
+    }
+}
---
+++ PuzzlesAndAlgorithms/ProjectEuler/MultiplesOf3And5.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PuzzlesAndAlgorithms.ProjectEuler
+{
+    /*
+     * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+     * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+     *
+     * Find the sum of all the multiples of 3 or 5 below 1000.
+     */
+    public class MultiplesOf3And5
+    {
+        public int GetMultiplesOf3And5(int bounds)
+        {
+            var values = new List<int>();
+
+            var multipleOf3 = 3;
+            var multipleOf5 = 5;
+
+            while (multipleOf3 < bounds)
+            {
+                values.Add(multipleOf3);
+                multipleOf3 += 3;
+
+            }
+
+            while (multipleOf5 < bounds)
+            {
+                values.Add(multipleOf5);
+                multipleOf5 += 5;
+            }
+
+            return values.Distinct().Sum();
+        }
+    }
+}
