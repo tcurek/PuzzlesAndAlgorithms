63b4c7c502b98504c7ac263a51c6237fdd9e2d0a
Tim Curek <tim.curek@almacgroup.com>
2018-12-20 20:17:14 -0500

    Exercise. solve fibanocci for x places

---
+++ PuzzleAndAlgorithms.Tests/Exercises/FibanocciTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesAndAlgorithms.Exercises;
+
+namespace PuzzlesAndAlgorithms.Tests.Exercises
+{
+    [TestClass()]
+    public class FibanocciTests
+    {
+        [TestMethod()]
+        public void GenerateFibanocciWhenBoundsLessThan2()
+        {
+            var sut = new Fibanocci();
+            var result = sut.GenerateFibanocci(1);
+            
+            CollectionAssert.AreEqual(new[]{0}, result);
+        }
+
+        [TestMethod()]
+        public void GenerateFibanocciWithBounds10()
+        {
+            var expected = new[] {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+            var sut = new Fibanocci();
+            var result = sut.GenerateFibanocci(10);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ PuzzlesAndAlgorithms/Exercises/Fibanocci.cs
@@ -0,0 +1,28 @@
+﻿namespace PuzzlesAndAlgorithms.Exercises
+{
+    public class Fibanocci
+    {
+        /*
+         * Each number is the sum of the 2 preceding numbers.
+         *
+         * Ex: 0 1 1 2 3 5 8 13
+         */
+
+        public int[] GenerateFibanocci(int numberOfRecords)
+        {
+            if (numberOfRecords < 2)
+                return new[]{0};
+
+            var result = new int[numberOfRecords];
+            result[0] = 0;
+            result[1] = 1;
+
+            for (var i = 2; i <= (numberOfRecords - 1); i++)
+            {
+                result[i] = result[i - 1] + result[i - 2];
+            }
+            
+            return result;
+        }
+    }
+}
