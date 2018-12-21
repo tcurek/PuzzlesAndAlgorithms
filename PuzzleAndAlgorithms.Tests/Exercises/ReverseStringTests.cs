3191db44698f4e9149f6e0bef676a98be92d4741
Tim Curek <tim.curek@almacgroup.com>
2018-12-20 21:37:01 -0500

    Exercise. Reverse String

---
+++ PuzzleAndAlgorithms.Tests/Exercises/ReverseStringTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesAndAlgorithms.Exercises;
+
+namespace PuzzlesAndAlgorithms.Tests.Exercises
+{
+    [TestClass()]
+    public class ReverseStringTests
+    {
+        [TestMethod()]
+        public void GetReversedStringWhenEmpty()
+        {
+            var result = string.Empty;
+            var sut = new ReverseString();
+            Assert.AreEqual(result, sut.GetReversedString(string.Empty));
+        }
+
+        [TestMethod()]
+        public void GetReversedString()
+        {
+            var result = "raB ooF";
+            var sut = new ReverseString();
+            Assert.AreEqual(result, sut.GetReversedString("Foo Bar"));
+        }
+    }
+}
---
+++ PuzzlesAndAlgorithms/Exercises/ReverseString.cs
@@ -0,0 +1,17 @@
+﻿namespace PuzzlesAndAlgorithms.Exercises
+{
+    public class ReverseString
+    {
+        public string GetReversedString(string input)
+        {
+            var result = "";
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                result += input[i];
+            }
+
+            return result;
+        }
+    }
+}
