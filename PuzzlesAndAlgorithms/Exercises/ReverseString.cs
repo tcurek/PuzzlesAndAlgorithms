04754cb87d90e6bb047167f52e778d16884b4c75
Tim Curek <tim.curek@almacgroup.com>
2018-12-20 21:39:24 -0500

    Exercise. Reverse String. Updated to use string builder

--- PuzzlesAndAlgorithms/Exercises/ReverseString.cs
+++ PuzzlesAndAlgorithms/Exercises/ReverseString.cs
@@ -1,17 +1,19 @@
-﻿namespace PuzzlesAndAlgorithms.Exercises
+﻿using System.Text;
+
+namespace PuzzlesAndAlgorithms.Exercises
 {
     public class ReverseString
     {
         public string GetReversedString(string input)
         {
-            var result = "";
+            var result = new StringBuilder();
 
             for (int i = input.Length - 1; i >= 0; i--)
             {
-                result += input[i];
+                result.Append(input[i]);
             }
 
-            return result;
+            return result.ToString();
         }
     }
 }
