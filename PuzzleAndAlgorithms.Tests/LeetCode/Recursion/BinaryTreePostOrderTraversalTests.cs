48b567f9d59b40b636473e7361e30ca4da67c02e
tcurek <timcurek@gmail.com>
2020-02-07 13:16:15 -0500

    Added LeetCode dir. First recursion problem.

---
+++ PuzzleAndAlgorithms.Tests/LeetCode/Recursion/BinaryTreePostOrderTraversalTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesAndAlgorithms.LeetCode.Recursion;
+using System.Collections.Generic;
+
+namespace PuzzlesAndAlgorithms.Tests.LeetCode.Recursion
+{
+    [TestClass()]
+    public class BinaryTreePostOrderTraversalTests
+    {
+        [TestMethod()]
+        public void Solution1PostOrderTraversal()
+        {
+            var expected = new List<int> {5, 1, 41, 8, 7};
+            var result = BinaryTreePostOrderTraversal.Solution1(GenerateTree1());
+            
+            Assert.AreEqual(expected.Count, result.Count);
+
+            for (int j = 0; j < expected.Count; j++)
+            {
+                Assert.AreEqual(expected[j], result[j]);
+            }
+        }
+
+        //[TestMethod()]
+        //public void Solution2PostOrderTraversal()
+        //{
+        //    var expected = new List<int> { 5, 1, 41, 8, 7 };
+        //    var result = BinaryTreePostOrderTraversal.Solution2(GenerateTree1());
+
+        //    Assert.AreEqual(expected.Count, result.Count);
+
+        //    for (int j = 0; j < expected.Count; j++)
+        //    {
+        //        Assert.AreEqual(expected[j], result[j]);
+        //    }
+        //}
+
+        private BinaryTreePostOrderTraversal.TreeNode GenerateTree1()
+        {
+            //      7
+            //  5       8
+            //      1       41
+            
+            var subRoot = new BinaryTreePostOrderTraversal.TreeNode(8) {left = new BinaryTreePostOrderTraversal.TreeNode(1), right = new BinaryTreePostOrderTraversal.TreeNode(41)};
+            var root = new BinaryTreePostOrderTraversal.TreeNode(7) {left = new BinaryTreePostOrderTraversal.TreeNode(5), right = subRoot};
+            return root;
+        }
+    }
+}
---
+++ PuzzlesAndAlgorithms/LeetCode/Recursion/BinaryTreePostOrderTraversal.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace PuzzlesAndAlgorithms.LeetCode.Recursion
+{
+    public static class BinaryTreePostOrderTraversal
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+         }
+
+        /* Given a binary tree, return the postorder traversal of its nodes' values.
+         *
+         * Input: [1,null,2,3]
+         * Output: [3,2,1]
+         */
+
+        // Solution 1
+
+        public static IList<int> Solution1(TreeNode root)
+        {
+            List<int> output = new List<int>();
+            PostTraverse(root, output);
+            return output;
+        }
+
+        private static void PostTraverse(TreeNode root, List<int> valList)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            PostTraverse(root.left, valList);
+            PostTraverse(root.right, valList);
+            valList.Add(root.val);
+        }
+
+        // Solution 2
+
+        //public static IList<int> Solution2(TreeNode root)
+        //{
+        //    Stack<int> rootStack = new Stack<int>();
+        //    var result = new List<int>();
+
+        //    if (root == null)
+        //    {
+        //        return result;
+        //    }
+
+        //    rootStack.Push(root.val);
+
+        //    if (root.left != null)
+        //    {
+        //        var left = Solution2(root.left);
+        //        foreach (var l in left)
+        //        {
+        //            rootStack.Push(l);
+        //        }
+        //    }
+
+        //    if (root.right != null)
+        //    {
+        //        var right = Solution2(root.right);
+        //        foreach (var r in right)
+        //        {
+        //            rootStack.Push(r);
+        //        }
+        //    }
+
+        //    while (rootStack.TryPop(out var d))
+        //    {
+        //        result.Add(d);
+        //    }
+            
+        //    return result;
+        //}
+    }
+}
