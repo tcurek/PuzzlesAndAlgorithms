f7199dacf73d0235c84b5b136eb7cd2bf59f6c4b
tcurek <timcurek@gmail.com>
2020-02-07 13:38:14 -0500

    LeetCode recursion intro problem.

---
+++ PuzzleAndAlgorithms.Tests/LeetCode/Recursion/ReverseStringTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesAndAlgorithms.LeetCode.Recursion;
+
+namespace PuzzlesAndAlgorithms.Tests.LeetCode.Recursion
+{
+    [TestClass()]
+    public class ReverseStringTests
+    {
+        [TestMethod()]
+        public void ReverseNormalString()
+        {
+            var input = "Tim";
+            var expected = "miT";
+
+            Assert.AreEqual(expected, RecursiveReverseString.Reverse(input));
+        }
+
+        [TestMethod()]
+        public void ReverseEmptyString()
+        {
+            var input = "";
+            var expected = "";
+
+            Assert.AreEqual(expected, RecursiveReverseString.Reverse(input));
+        }
+
+    }
+}
---
+++ PuzzlesAndAlgorithms/LeetCode/Recursion/RecursiveReverseString.cs
@@ -0,0 +1,31 @@
+﻿namespace PuzzlesAndAlgorithms.LeetCode.Recursion
+{
+    public static class RecursiveReverseString
+    {
+        /*
+         * Using recursion. reverse the characters in the string parameter.
+         *
+         *  Param: Tim
+         *  returns: miT
+         *
+         *  ** Dont use string builder... ignore for this case
+         */
+         
+        public static string Reverse(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+
+            var lastChar = str[str.Length - 1].ToString();
+
+            if (str.Length > 1)
+            {
+                lastChar += Reverse(str.Substring(0, str.Length - 1));
+            }
+
+            return lastChar;
+        }
+    }
+}
