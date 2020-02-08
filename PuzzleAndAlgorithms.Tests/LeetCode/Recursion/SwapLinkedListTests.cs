b43fb4795a0b4515210724e9f3a474fcfa5e2f7d
tcurek <timcurek@gmail.com>
2020-02-08 14:11:43 -0500

    LeetCode. Recursion. Swap nodes.

---
+++ PuzzleAndAlgorithms.Tests/LeetCode/Recursion/SwapLinkedListTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesAndAlgorithms.LeetCode.Recursion;
+
+namespace PuzzlesAndAlgorithms.Tests.LeetCode.Recursion
+{
+    [TestClass()]
+    public class SwapLinkedListTests
+    {
+        [TestMethod()]
+        public void EvenLinkedList()
+        {
+            var node4 = new SwapLinkedList.ListNode { val = 4};
+            var node3 = new SwapLinkedList.ListNode { val = 3, next = node4};
+            var node2 = new SwapLinkedList.ListNode { val = 2, next = node3};
+            var head = new SwapLinkedList.ListNode { val = 1, next = node2};
+
+            var sut = new SwapLinkedList.Solution();
+            var result = sut.SwapPairs(head);
+
+            Assert.AreEqual(2, result.val);
+            Assert.AreEqual(1, result.next.val);
+            Assert.AreEqual(4, result.next.next.val);
+            Assert.AreEqual(3, result.next.next.next.val);
+        }
+    }
+}
---
+++ PuzzlesAndAlgorithms/LeetCode/Recursion/SwapLinkedList.cs
@@ -0,0 +1,35 @@
+﻿namespace PuzzlesAndAlgorithms.LeetCode.Recursion
+{
+    public static class SwapLinkedList
+    {
+        public class ListNode
+        {
+            public ListNode() {}
+            public ListNode(int x) { val = x; }
+            public int val;
+            public ListNode next;
+         }
+         
+        /*
+         * Given a linked list, swap every two adjacent nodes and return its head.
+         * e.g.  for a list 1-> 2 -> 3 -> 4, one should return the head of list as 2 -> 1 -> 4 -> 3.
+         */
+
+        public class Solution
+        {
+            public ListNode SwapPairs(ListNode head)
+            {
+                if (head == null || head.next == null)
+                {
+                    return head;
+                }
+
+                var newHeadNode = head.next;
+                head.next = SwapPairs(head.next.next);
+                newHeadNode.next = head;
+
+                return newHeadNode;
+            }
+        }
+    }
+}
